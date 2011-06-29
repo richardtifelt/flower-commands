4c34697f32d36d0c835a07bba2b6853fcb11625b
David Wennergren <david@david.local>
2011-06-29 17:20:07 +0200

    Add some wicked capabilities to the say command

--- commands/say.rb
+++ commands/say.rb
@@ -1,8 +1,15 @@
 class Say < Flower::Command
-  respond_to "say"
+  respond_to "say", "whisper", "sing"
   
   def self.respond(command, message, sender, flower)
-    system "say", message
+    case command
+    when "whisper"
+      system "say", "-vwhisper", message
+    when "sing"
+      system "say", "-vcello", message  
+    when "say"
+      system "say", message  
+    end
   end
 
   def self.description
